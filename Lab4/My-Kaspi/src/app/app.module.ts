b0633784bcfb2846fa1eb88f86e40aa3329cb638
Malia-K <98634931+Malia-K@users.noreply.github.com>
2023-02-22 14:24:42 +0600

    :white_check_mark: Finished kaspi task

--- Lab4/My-Kaspi/src/app/app.module.ts
+++ Lab4/My-Kaspi/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -14,7 +14,11 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productName', component: ProductDetailsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
