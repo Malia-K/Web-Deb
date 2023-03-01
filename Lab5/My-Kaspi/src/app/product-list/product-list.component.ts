15975c9f984513ea680008d1a30452833e1f5250
Malia-K <98634931+Malia-K@users.noreply.github.com>
2023-03-01 09:57:45 +0600

    :white_check_mark: like and delete buttons

--- Lab5/My-Kaspi/src/app/product-list/product-list.component.ts
+++ Lab5/My-Kaspi/src/app/product-list/product-list.component.ts
@@ -30,6 +30,16 @@ export class ProductListComponent {
     console.log(this.selectedCategory)
     this.filterByCategory();
   }
+
+  incrementLikes(id : number) {
+    products.filter(product => product.id === id).map(product => product.like += 1)
+  }
+
+
+  deleteProduct(id : number) {
+    this.allproducts = this.allproducts.filter(product => product.id !== id);
+    this.filterByCategory();
+  }
 }
 
 
